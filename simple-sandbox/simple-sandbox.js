c09f4880920964980ec0bb62190f3b5e1e77ed83
Hornet07 <null>
2023-06-04 08:59:30 +0300

    renderer complete

--- simple-sandbox/simple-sandbox.js
+++ simple-sandbox/simple-sandbox.js
@@ -23,6 +23,15 @@ function makeSandbox(canvas) {
             this.objects.forEach(object => {
                 object.renderer.draw(object, this.surface);
             });
+        },
+        drawLine: function(from, to, width = 5) {
+            const sw = this.surface.canvas.width, sh = this.surface.canvas.height;
+            this.surface.beginPath();
+            this.surface.lineWidth = width/unit*(0.5*(sw+sh));
+            this.surface.moveTo((from.x+unit*0.5)/unit*sw, (from.y+unit*0.5)/unit*sh);
+            this.surface.lineTo((to.x+unit*0.5)/unit*sw, (to.y+unit*0.5)/unit*sh);
+            this.surface.stroke();
+            this.surface.closePath();
         }
     };
 }
@@ -63,6 +72,42 @@ function boxRenderer(width, height) {
                 (transform.position.x-nw*0.5)*sw, (transform.position.y-nh*0.5)*sh,
                 nw*sw, nh*sh
             );
+
+            surface.translate((transform.position.x)*sw, (transform.position.y)*sh);
+            surface.rotate(-transform.rotation*(Math.PI/180));
+            surface.translate(-(transform.position.x)*sw, -(transform.position.y)*sh);
+        }
+    };
+}
+
+function circleRenderer(radius) {
+    return {
+        draw: function(self, surface) {
+            const transform = self.transform.toNormalizedCamera();
+            const sw = surface.canvas.width, sh = surface.canvas.height;
+            const nr = radius/unit*(0.5*(transform.scale.x+transform.scale.y));
+
+            surface.beginPath();
+            surface.arc((transform.position.x)*sw, (transform.position.y)*sh, Math.abs(nr*(0.5*(sw+sh))), 0, Math.PI*2);
+            surface.stroke();
+            surface.fill();
+            surface.closePath();
+        }
+    };
+}
+
+function textureRenderer(img, width, height) {
+    return {
+        draw: function(self, surface) {
+            const transform = self.transform.toNormalizedCamera();
+            const nw = width/unit*transform.scale.x, nh = height/unit*transform.scale.y;
+            const sw = surface.canvas.width, sh = surface.canvas.height;
+            
+            surface.translate((transform.position.x)*sw, (transform.position.y)*sh);
+            surface.rotate(transform.rotation*(Math.PI/180));
+            surface.translate(-(transform.position.x)*sw, -(transform.position.y)*sh);
+
+            surface.drawImage(img, (transform.position.x-nw*0.5)*sw, (transform.position.y-nh*0.5)*sh, nw*sw, nh*sh);
 
             surface.translate((transform.position.x)*sw, (transform.position.y)*sh);
             surface.rotate(-transform.rotation*(Math.PI/180));
