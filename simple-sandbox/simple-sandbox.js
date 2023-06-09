e92afd3d53b986e80d9315561ed3aea9de792539
Hornet07 <null>
2023-06-09 09:07:41 +0300

    latest

--- simple-sandbox/simple-sandbox.js
+++ simple-sandbox/simple-sandbox.js
@@ -1,9 +1,9 @@
 function makeSandbox(canvas) {
-    return {
+    let sandbox = {
         clock: 0,
         surface: canvas.getContext("2d"),
         objects: new Map(),
-        makeObject: function(renderer, collider, physics) {
+        makeObject: function (renderer, collider, physics) {
             const id = this.clock++;
             this.objects.set(id, {
                 id: id,
@@ -15,25 +15,39 @@ function makeSandbox(canvas) {
             });
             return this.objects.get(id);
         },
-        removeObject: function(object) {
+        removeObject: function (object) {
             this.objects.delete(object.id);
         },
-        update: function() {
+        update: function () {
             this.surface.reset();
             this.objects.forEach(object => {
                 object.renderer.draw(object, this.surface);
             });
         },
-        drawLine: function(from, to, width = 5) {
+        drawLine: function (from, to, width = 5) {
             const sw = this.surface.canvas.width, sh = this.surface.canvas.height;
             this.surface.beginPath();
-            this.surface.lineWidth = width/unit*(0.5*(sw+sh));
-            this.surface.moveTo((from.x+unit*0.5)/unit*sw, (from.y+unit*0.5)/unit*sh);
-            this.surface.lineTo((to.x+unit*0.5)/unit*sw, (to.y+unit*0.5)/unit*sh);
+            this.surface.lineWidth = width / unit * (0.5 * (sw + sh));
+            if (sw > sh) {
+                this.surface.moveTo((from.x + unit * 0.5) / unit * sh + (sw - sh) * 0.5, (from.y + unit * 0.5) / unit * sh);
+                this.surface.lineTo((to.x + unit * 0.5) / unit * sh + (sw - sh) * 0.5, (to.y + unit * 0.5) / unit * sh);
+            } else if (sw < sh) {
+                this.surface.moveTo((from.x + unit * 0.5) / unit * sw, (from.y + unit * 0.5) / unit * sw + (sh - sw) * 0.5);
+                this.surface.lineTo((to.x + unit * 0.5) / unit * sw, (to.y + unit * 0.5) / unit * sw + (sh - sw) * 0.5);
+            } else {
+                this.surface.moveTo((from.x + unit * 0.5) / unit * sw, (from.y + unit * 0.5) / unit * sh);
+                this.surface.lineTo((to.x + unit * 0.5) / unit * sw, (to.y + unit * 0.5) / unit * sh);
+            }
             this.surface.stroke();
             this.surface.closePath();
         }
     };
+    sandbox.surface.rotateFrom = function (position, angle) {
+        this.translate(position.x, position.y);
+        this.rotate(angle);
+        this.translate(-position.x, -position.y);
+    };
+    return sandbox;
 }
 
 const unit = 1000;
@@ -47,48 +61,53 @@ function transform() {
         position: vec(0, 0),
         scale: vec(1, 1),
         rotation: 0,
-        toNormalizedCamera: function() {
-            return {
-                position: vec((this.position.x+unit*0.5)/unit, (this.position.y+unit*0.5)/unit),
-                scale: this.scale,
-                rotation: this.rotation,
-            };
+        toNormalizedUnits: function () {
+            let clone = transform();
+            clone.position = vec((this.position.x + unit * 0.5) / unit, (this.position.y + unit * 0.5) / unit);
+            clone.scale = vec(this.scale.x, this.scale.y);
+            clone.rotation = this.rotation;
+            return clone;
+        },
+        toViewport: function (surface) {
+            const sw = surface.canvas.width, sh = surface.canvas.height;
+            let transform = this.toNormalizedUnits();
+            if (sw > sh) {
+                transform.position = vec(transform.position.x * sh + (sw - sh) * 0.5, transform.position.y * sh);
+                transform.scale.x /= sw / sh;
+            } else if (sw < sh) {
+                transform.position = vec(transform.position.x * sw, transform.position.y * sw + (sh - sw) * 0.5);
+                transform.scale.y /= sh / sw;
+            } else {
+                transform.position = vec(transform.position.x * sw, transform.position.y * sh);
+            }
+            return transform;
         }
     };
 }
 
 function boxRenderer(width, height) {
     return {
-        draw: function(self, surface) {
-            const transform = self.transform.toNormalizedCamera();
-            const nw = width/unit*transform.scale.x, nh = height/unit*transform.scale.y;
+        draw: function (self, surface) {
+            const transform = self.transform.toViewport(surface);
+            const nw = width / unit * transform.scale.x, nh = height / unit * transform.scale.y;
             const sw = surface.canvas.width, sh = surface.canvas.height;
-            
-            surface.translate((transform.position.x)*sw, (transform.position.y)*sh);
-            surface.rotate(transform.rotation*(Math.PI/180));
-            surface.translate(-(transform.position.x)*sw, -(transform.position.y)*sh);
 
-            surface.fillRect(
-                (transform.position.x-nw*0.5)*sw, (transform.position.y-nh*0.5)*sh,
-                nw*sw, nh*sh
-            );
-
-            surface.translate((transform.position.x)*sw, (transform.position.y)*sh);
-            surface.rotate(-transform.rotation*(Math.PI/180));
-            surface.translate(-(transform.position.x)*sw, -(transform.position.y)*sh);
+            surface.rotateFrom(transform.position, transform.rotation * (Math.PI / 180));
+            surface.fillRect(transform.position.x - (nw * 0.5 * sw), transform.position.y - (nh * 0.5 * sh), nw * sw, nh * sh);
+            surface.rotateFrom(transform.position, -transform.rotation * (Math.PI / 180));
         }
     };
 }
 
-function circleRenderer(radius) {
+function sphereRenderer(radius) {
     return {
-        draw: function(self, surface) {
-            const transform = self.transform.toNormalizedCamera();
+        draw: function (self, surface) {
+            const transform = self.transform.toViewport(surface);
             const sw = surface.canvas.width, sh = surface.canvas.height;
-            const nr = radius/unit*(0.5*(transform.scale.x+transform.scale.y));
+            const nr = radius / unit * (0.5 * (transform.scale.x + transform.scale.y));
 
             surface.beginPath();
-            surface.arc((transform.position.x)*sw, (transform.position.y)*sh, Math.abs(nr*(0.5*(sw+sh))), 0, Math.PI*2);
+            surface.arc(transform.position.x, transform.position.y, Math.abs(nr * (0.5 * (sw + sh))), 0, Math.PI * 2);
             surface.stroke();
             surface.fill();
             surface.closePath();
@@ -98,20 +117,110 @@ function circleRenderer(radius) {
 
 function textureRenderer(img, width, height) {
     return {
-        draw: function(self, surface) {
-            const transform = self.transform.toNormalizedCamera();
-            const nw = width/unit*transform.scale.x, nh = height/unit*transform.scale.y;
+        draw: function (self, surface) {
+            const transform = self.transform.toViewport(surface);
+            const nw = width / unit * transform.scale.x, nh = height / unit * transform.scale.y;
             const sw = surface.canvas.width, sh = surface.canvas.height;
+
+            surface.rotateFrom(transform.position, transform.rotation * (Math.PI / 180));
+            surface.drawImage(img, transform.position.x - (nw * 0.5 * sw), transform.position.y - (nh * 0.5 * sh), nw * sw, nh * sh);
+            surface.rotateFrom(transform.position, -transform.rotation * (Math.PI / 180));
+        }
+    };
+}
+
+//This was HELL to implement
+function orientedBoxCollider(width, height) {
+    return {
+        width: width,
+        height: height,
+        checkCollision: function (self, other) {
+            const selfVertices = getTransformedVertices(self.transform, width, height);
+            const otherVertices = getTransformedVertices(other.transform, other.collider.width, other.collider.height);
             
-            surface.translate((transform.position.x)*sw, (transform.position.y)*sh);
-            surface.rotate(transform.rotation*(Math.PI/180));
-            surface.translate(-(transform.position.x)*sw, -(transform.position.y)*sh);
+            return checkSATCollision(selfVertices, otherVertices);
+        }
+    };
+}
 
-            surface.drawImage(img, (transform.position.x-nw*0.5)*sw, (transform.position.y-nh*0.5)*sh, nw*sw, nh*sh);
+function getTransformedVertices(transform, width, height) {
+    const position = transform.position;
+    const scale = transform.scale;
+    const rotation = transform.rotation;
 
-            surface.translate((transform.position.x)*sw, (transform.position.y)*sh);
-            surface.rotate(-transform.rotation*(Math.PI/180));
-            surface.translate(-(transform.position.x)*sw, -(transform.position.y)*sh);
+    const halfWidth = width * 0.5;
+    const halfHeight = height * 0.5;
+
+    const vertices = [
+        vec(position.x - halfWidth * scale.x, position.y - halfHeight * scale.y), //bottom left
+        vec(position.x + halfWidth * scale.x, position.y - halfHeight * scale.y), //bottom right
+        vec(position.x + halfWidth * scale.x, position.y + halfHeight * scale.y), //top right
+        vec(position.x - halfWidth * scale.x, position.y + halfHeight * scale.y) //top left
+    ];
+
+    const rotatedVertices = vertices.map(vertex => {
+        const cosTheta = Math.cos(rotation * (Math.PI / 180));
+        const sinTheta = Math.sin(rotation * (Math.PI / 180));
+
+        const translatedX = vertex.x - position.x;
+        const translatedY = vertex.y - position.y;
+
+        const rotatedX = translatedX * cosTheta - translatedY * sinTheta;
+        const rotatedY = translatedX * sinTheta + translatedY * cosTheta;
+
+        return vec(rotatedX + position.x, rotatedY + position.y);
+    });
+
+    return rotatedVertices;
+}
+
+
+function dotProduct(vec1, vec2) {
+    return vec1.x * vec2.x + vec1.y * vec2.y;
+}
+
+//Works with any convex mesh
+function checkSATCollision(vertices1, vertices2) {
+    for (let i = 0; i < vertices1.length; i++) {
+        const point1 = vertices1[i];
+        const point2 = vertices1[(i + 1) % vertices1.length];
+
+        const edge = vec(point2.x - point1.x, point2.y - point1.y);
+        const axis = vec(-edge.y, edge.x);
+
+        const axisLength = Math.sqrt(dotProduct(axis, axis));
+        const normalizedAxis = vec(axis.x / axisLength, axis.y / axisLength);
+
+        let min1 = Number.MAX_VALUE;
+        let max1 = Number.MIN_VALUE;
+        let min2 = Number.MAX_VALUE;
+        let max2 = Number.MIN_VALUE;
+
+        for (let j = 0; j < vertices1.length; j++) {
+            const projectedPoint = dotProduct(vertices1[j], normalizedAxis);
+            min1 = Math.min(min1, projectedPoint);
+            max1 = Math.max(max1, projectedPoint);
+        }
+
+        for (let j = 0; j < vertices2.length; j++) {
+            const projectedPoint = dotProduct(vertices2[j], normalizedAxis);
+            min2 = Math.min(min2, projectedPoint);
+            max2 = Math.max(max2, projectedPoint);
+        }
+
+        if (max1 < min2 || max2 < min1) return false;
+    }
+    return true;
+}
+
+function sphereCollider(radius) {
+    return {
+        radius: radius,
+        checkCollision: function (self, other) {
+            const selfPosition = self.transform.position;
+            const otherPosition = other.transform.position;
+            const distance = Math.sqrt(Math.pow(selfPosition.x - otherPosition.x, 2) + Math.pow(selfPosition.y - otherPosition.y, 2));
+            return distance <= this.radius + other.collider.radius;
         }
     };
 }
